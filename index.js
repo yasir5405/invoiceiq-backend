3b899d4d1555693d3010c98c8fac5bec98997438
yasir5405 <yasirnaseem5415@gmail.com>
2025-07-09 19:19:32 +0530

    add binding to '0.0.0.0'

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", userRouter);
 app.use("/auth", authRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
